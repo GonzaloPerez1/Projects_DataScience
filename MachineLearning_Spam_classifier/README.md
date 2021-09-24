85b5d0bdb2d20c7a234e565c2a4f434624e0403f
GonzaloPerez1 <71379299+GonzaloPerez1@users.noreply.github.com>
2021-09-24 12:22:33 +0200

    Delete MachineLearning_Spam_classifier directory

--- MachineLearning_Spam_classifier/README.md
+++
@@ -1,8 +0,0 @@
-# Spam Classifier
-
-## Installation:
-- pip install requirements.txt -r
-
-- #Ir al directorio que contenga main.py
-
-- python main.py
--- MachineLearning_Spam_classifier/src/info.py
+++
@@ -1,5 +0,0 @@
-'''
-Aquí encontraremos las variables estáticas.
-'''
-
-CSV_ROUTE = 'https://raw.githubusercontent.com/amankharwal/SMS-Spam-Detection/master/spam.csv'
